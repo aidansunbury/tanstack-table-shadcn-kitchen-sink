c10d758e0eb39be797f33b3991a1d0e3b47cbcf7
aidansunbury <aidansunbury@gmail.com>
2024-08-18 20:00:06 -0700

    made some progress

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# Example
+# Tanstack table kitchen sink
 
-To run this example:
+This example is built off of the [Tanstack Table Kitchen Sink Example](https://tanstack.com/table/latest/docs/framework/react/examples/kitchen-sink) in the docs, but also includes [sticky column pinning](https://tanstack.com/table/latest/docs/framework/react/examples/column-pinning-sticky).
 
-- `npm install` or `yarn`
-- `npm run start` or `yarn start`
+More importantly, it is refactored to have a nice ui with shadcn components, and be styled with tailwindcss, instead of the example which uses native css.
+
+One final important thing, is that is shows how to dynamically generate the table without having to hardcode the columns.
--- src/components/CustomTable.tsx
+++ src/components/CustomTable.tsx
@@ -73,119 +73,125 @@ export function CustomTable<T extends RowData>({
 	};
 
 	return (
-		<table>
-			<thead>
-				{headerGroups.map((headerGroup) => (
-					<tr key={headerGroup.id}>
-						{headerGroup.headers.map((header) => {
-							const { column } = header;
+		<div className="table-container">
+			<table
+				style={{
+					width: table.getTotalSize(),
+				}}
+			>
+				<thead>
+					{headerGroups.map((headerGroup) => (
+						<tr key={headerGroup.id}>
+							{headerGroup.headers.map((header) => {
+								const { column } = header;
 
-							return (
-								<th
-									className="relative"
-									key={header.id}
-									// style={{
-									// 	width: header.getSize(),
-									// }}
-									style={{ ...getCommonPinningStyles(column) }}
-									colSpan={header.colSpan}
-								>
-									{header.isPlaceholder ? null : (
-										<>
-											<div>
-												{header.column.getCanGroup() ? (
-													// If the header can be grouped, let's add a toggle
-													<button
-														onClick={header.column.getToggleGroupingHandler()}
-														style={{
-															cursor: "pointer",
-														}}
-													>
-														{header.column.getIsGrouped()
-															? `🛑(${header.column.getGroupedIndex()})`
-															: `👊`}
-													</button>
-												) : null}{" "}
-												{flexRender(
-													header.column.columnDef.header,
-													header.getContext(),
-												)}{" "}
-												<button
-													onClick={header.column.getToggleSortingHandler()}
-													className={
-														header.column.getCanSort()
-															? "cursor-pointer select-none"
-															: ""
-													}
-												>
-													{{
-														asc: "🔼",
-														desc: "🔽",
-													}[header.column.getIsSorted() as string] ?? "📶"}
-												</button>
-											</div>
-											{header.column.getCanFilter() ? (
+								return (
+									<th
+										className="relative"
+										key={header.id}
+										// style={{
+										// 	width: header.getSize(),
+										// }}
+										style={{ ...getCommonPinningStyles(column) }}
+										colSpan={header.colSpan}
+									>
+										{header.isPlaceholder ? null : (
+											<>
 												<div>
-													<Filter column={header.column} table={table} />
+													{header.column.getCanGroup() ? (
+														// If the header can be grouped, let's add a toggle
+														<button
+															onClick={header.column.getToggleGroupingHandler()}
+															style={{
+																cursor: "pointer",
+															}}
+														>
+															{header.column.getIsGrouped()
+																? `🛑(${header.column.getGroupedIndex()})`
+																: `👊`}
+														</button>
+													) : null}{" "}
+													{flexRender(
+														header.column.columnDef.header,
+														header.getContext(),
+													)}{" "}
+													<button
+														onClick={header.column.getToggleSortingHandler()}
+														className={
+															header.column.getCanSort()
+																? "cursor-pointer select-none"
+																: ""
+														}
+													>
+														{{
+															asc: "🔼",
+															desc: "🔽",
+														}[header.column.getIsSorted() as string] ?? "📶"}
+													</button>
 												</div>
-											) : null}
-										</>
-									)}
-									<div
-										className="absolute right-0 top-0 h-full w-1 bg-blue-300 select-none touch-none hover:bg-blue-500 cursor-col-resize"
-										onMouseDown={header.getResizeHandler()}
-										onTouchStart={header.getResizeHandler()}
-									/>
-									{!header.isPlaceholder && header.column.getCanPin() && (
-										<TablePins
-											isPinned={header.column.getIsPinned()}
-											pin={header.column.pin}
-										/>
-									)}
-								</th>
-							);
-						})}
-					</tr>
-				))}
-			</thead>
-			<tbody>
-				{table.getRowModel().rows.map((row) => (
-					<tr key={row.id}>
-						{getRowGroup(row, tableGroup).map((cell) => {
-							const { column } = cell;
-
-							return (
-								<td
-									key={cell.id}
-									// style={{
-									// 	width: cell.column.getSize(),
-									// }}
-									style={{ ...getCommonPinningStyles(column) }}
-								>
-									{flexRender(cell.column.columnDef.cell, cell.getContext())}
-								</td>
-							);
-						})}
-					</tr>
-				))}
-			</tbody>
-			<tfoot>
-				{footerGroup.map((footerGroup) => (
-					<tr key={footerGroup.id}>
-						{footerGroup.headers.map((header) => (
-							<th key={header.id} colSpan={header.colSpan}>
-								{header.isPlaceholder
-									? null
-									: flexRender(
-											header.column.columnDef.footer,
-											header.getContext(),
+												{header.column.getCanFilter() ? (
+													<div>
+														<Filter column={header.column} table={table} />
+													</div>
+												) : null}
+											</>
 										)}
-							</th>
-						))}
-					</tr>
-				))}
-			</tfoot>
-		</table>
+										<div
+											className="absolute right-0 top-0 h-full w-1 bg-blue-300 select-none touch-none hover:bg-blue-500 cursor-col-resize"
+											onMouseDown={header.getResizeHandler()}
+											onTouchStart={header.getResizeHandler()}
+										/>
+										{!header.isPlaceholder && header.column.getCanPin() && (
+											<TablePins
+												isPinned={header.column.getIsPinned()}
+												pin={header.column.pin}
+											/>
+										)}
+									</th>
+								);
+							})}
+						</tr>
+					))}
+				</thead>
+				<tbody>
+					{table.getRowModel().rows.map((row) => (
+						<tr key={row.id}>
+							{getRowGroup(row, tableGroup).map((cell) => {
+								const { column } = cell;
+
+								return (
+									<td
+										key={cell.id}
+										// style={{
+										// 	width: cell.column.getSize(),
+										// }}
+										style={{ ...getCommonPinningStyles(column) }}
+									>
+										{flexRender(cell.column.columnDef.cell, cell.getContext())}
+									</td>
+								);
+							})}
+						</tr>
+					))}
+				</tbody>
+				<tfoot>
+					{footerGroup.map((footerGroup) => (
+						<tr key={footerGroup.id}>
+							{footerGroup.headers.map((header) => (
+								<th key={header.id} colSpan={header.colSpan}>
+									{header.isPlaceholder
+										? null
+										: flexRender(
+												header.column.columnDef.footer,
+												header.getContext(),
+											)}
+								</th>
+							))}
+						</tr>
+					))}
+				</tfoot>
+			</table>
+		</div>
 	);
 }
 
